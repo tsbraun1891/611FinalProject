ae00879237784c0354b8375ecf28460ddd38c942
Ling <cathyxie@brandeis.edu>
2020-12-04 16:48:33 -0500

    GUIHome finished
    as above

--- GUIHome.java
+++ GUIHome.java
@@ -1,9 +1,40 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+
 
 public class GUIHome {
 
 	public GUIHome() {
 		// TODO Auto-generated constructor stub
-	}
+		JFrame frame = new JFrame();
+		JButton button = new JButton("Start");
+		button.setBounds(350, 450, 80, 25);
+		button.addActionListener(new ActionListener() {
 
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				GUILogin login = new GUILogin();
+				frame.dispose();
+			}
+			
+		});
+		frame.add(button);
+		
+		ImageIcon icon = new ImageIcon("Welcome.jpg");
+		JLabel label = new JLabel();
+		label.setIcon(icon);
+		frame.getContentPane().add(label);
+		
+		frame.setSize(500,500);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+	
 }
