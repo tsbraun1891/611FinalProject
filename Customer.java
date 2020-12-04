58bb07112e0e62672fa1371e21b32a8706433ca6
Xinyi Zhao <zxy1307@bu.edu>
2020-12-03 23:17:25 -0500

    User class

---
+++ Customer.java
@@ -0,0 +1,11 @@
+
+public class Customer extends User{
+
+	
+	public Customer(int userId,String firstName, String lastName, String userName, String password, double balance) {
+		super(userId, firstName, lastName, userName, password, balance);
+		
+	}
+	
+	
+}
---
+++ User.java
@@ -0,0 +1,64 @@
+
+public class User {
+
+	protected int userId;
+	protected String firstName;
+	protected String lastName;
+	protected String userName;
+	protected String password;
+	protected double balance;
+
+	public User(int userId, String fistName, String lastName, String username, String password, double balance) {
+		setUserId(userId);
+		setFirstName(fistName);
+		setLastName(lastName);
+		setUserName(username);
+		setPassword(password);
+		setBalance(balance);
+	}
+	
+	//getter and setter for user
+	public void setUserId(int id) {
+		this.userId = id;
+	}
+	public int getUserId() {
+		return this.userId;
+	}
+	
+	public void setFirstName(String first) {
+		this.firstName = first;
+	}
+	public String getFirstName() {
+		return this.firstName;
+	}
+	
+	public void setLastName(String last) {
+		this.lastName = last;
+	}
+	public String getLastName() {
+		return this.lastName;
+	}
+	
+	public void setUserName(String user) {
+		this.userName = user;
+	}
+	public String getUserName() {
+		return this.userName;
+	}
+	
+	public void setPassword(String psw) {
+		this.password = psw;
+	}
+	public String getPassword() {
+		return this.password;
+	}
+	
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	public double getBalance() {
+		return this.balance;
+	}
+	
+	
+}
