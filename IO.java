e1f48c46f40e5e373db174c68edb3154bcd21e3e
tsbraun1891 <tsbraun1891@gmail.com>
2020-12-05 13:27:16 -0500

    Created IO class

--- IO.java
+++ IO.java
@@ -1,4 +1,33 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
 
 public class IO {
 
+    public IO() {
+
+    }
+
+    /**
+     * Reads a file and returns an array of all of the lines in the file
+     * @param fileName - name of the file
+     * @return string arraylist of all lines of the given file
+     */
+    public ArrayList<String> readFile(String fileName) {
+        ArrayList<String> rhet = new ArrayList<String>();
+        String row;
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            while((row = reader.readLine()) != null) {
+                rhet.add(row);
+            }
+
+            reader.close();
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+
+        return rhet;
+    }
 }
