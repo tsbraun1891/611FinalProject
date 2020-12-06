afbeaec169ec84a47217d83d729163ee83f03cb7
Ling <cathyxie@brandeis.edu>
2020-12-06 17:18:58 -0500

    finished GUICustomerHome, GUIAdminHome, GUIQuit
    as above

--- GUIAdminHome.java
+++ GUIAdminHome.java
@@ -1,4 +1,86 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class GUIAdminHome {
+	private JFrame frame;
+	private JButton dailyReportButton;
+	private JButton checkCustomerButton;
+	private JButton quitButton;
+	
+	public GUIAdminHome() {
+		frame = new JFrame();
+		dailyReportButton = new JButton("Get Daily Report");
+		dailyReportButton.setBounds(300, 210, 200, 25);
+		addDailyReportButtonFunction();
+		frame.add(dailyReportButton);
+		
+		checkCustomerButton = new JButton("Check Customer");//????????????????????
+		checkCustomerButton.setBounds(300, 240, 200, 25);
+		addCheckCustomerButtonFunction();
+		frame.add(checkCustomerButton);
+		
+		quitButton = new JButton("Quit");
+		quitButton.setBounds(710, 500, 80, 25);
+		addQuitButtonFunction();
+		frame.add(quitButton);
+		
+		addBackgroundPic();
+		
+		frame.setSize(800,550);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
 
+	private void addQuitButtonFunction() {
+		// TODO Auto-generated method stub
+		quitButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				GUIQuit quit = new GUIQuit();
+				closeFrame();
+			}		
+		});
+	}
+
+	private void addCheckCustomerButtonFunction() {
+		// TODO Auto-generated method stub
+		checkCustomerButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				GUICheckCustomer check = new GUICheckCustomer();
+				closeFrame();
+			}		
+		});
+	}
+
+	private void addDailyReportButtonFunction() {
+		// TODO Auto-generated method stub
+		dailyReportButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				GUIDailyReport report = new GUIDailyReport();
+				closeFrame();
+			}		
+		});
+	}
+	
+	public void addBackgroundPic() {
+		ImageIcon icon = new ImageIcon("bg.jpg");
+		JLabel label = new JLabel();
+		label.setIcon(icon);
+		frame.getContentPane().add(label);
+	}
+	
+	public void closeFrame() {
+		frame.dispose();
+	}
+	
 }
---
+++ GUIDailyReport.java
@@ -0,0 +1,4 @@
+
+public class GUIDailyReport {
+
+}
