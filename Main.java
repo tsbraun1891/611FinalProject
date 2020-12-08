0c70fdae0c3989a80ced461238debddb01ef29fd
Ling <cathyxie@brandeis.edu>
2020-12-08 13:30:11 -0500

    finished GUIDeposit
    as above

--- Main.java
+++ Main.java
@@ -3,7 +3,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		GUIHome h = new GUIHome();
+		//GUIHome h = new GUIHome();
+		GUIDeposit d = new GUIDeposit();
 	}
 
 }
